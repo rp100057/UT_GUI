b794addb5efe78e1a361e23b2e27b1516e317c31
rp100057 <rp100057@fh-muenster.de>
2017-01-31 09:53:22 +0100

    Added slicer
    Matlab script to slice *.STL files

---
+++ Slicer/generate_slices.m
@@ -0,0 +1,85 @@
+clc
+close all
+clear all
+%% INPUT
+
+stl_file='Part1.stl';
+slice_direction='z';
+print_displacement=500e-6; %m
+layer_displacement=1e-3; %m
+
+%% Receive dimension from STL file
+
+%Plot the original STL mesh:
+figure
+[stlcoords] = READ_stl(stl_file);
+xco = squeeze( stlcoords(:,1,:) )';
+yco = squeeze( stlcoords(:,2,:) )';
+zco = squeeze( stlcoords(:,3,:) )';
+[hpat] = patch(xco,yco,zco,'b');
+axis equal
+
+
+xco=max(reshape(xco,[],1)*1e-3); %m
+yco=max(reshape(yco,[],1)*1e-3); %m
+zco=max(reshape(zco,[],1)*1e-3); %m
+
+switch slice_direction
+     case 'x'
+        xco=xco-mod(xco,layer_displacement);
+        yco=yco-mod(yco,print_displacement);
+        zco=zco-mod(zco,print_displacement);
+     case 'y'
+        xco=xco-mod(xco,print_displacement);
+        yco=yco-mod(yco,layer_displacement);
+        zco=zco-mod(zco,print_displacement);
+     case 'z'
+        xco=xco-mod(xco,print_displacement);
+        yco=yco-mod(yco,print_displacement);
+        zco=zco-mod(zco,layer_displacement);
+end
+
+%% Generate and save VOXEL-map from STL
+
+switch slice_direction
+     case 'x'
+        %Voxelise the STL:
+        xx=xco/layer_displacement;      
+        yy=yco/print_displacement;
+        zz=zco/print_displacement;
+        [OUTPUTgrid] = VOXELISE(round(xx),round(yy),round(zz),stl_file,'xyz');  
+        for i=1:xx
+            figure
+            imagesc(squeeze(OUTPUTgrid(i,:,:)))
+            colormap(gray(2))
+            axis equal tight
+            imwrite(squeeze(OUTPUTgrid(i,:,:)),['output/array',num2str(i),'.png'])
+        end
+        
+    case 'y'
+        %Voxelise the STL:
+        xx=xco/print_displacement;
+        yy=yco/layer_displacement;
+        zz=zco/print_displacement;
+        [OUTPUTgrid] = VOXELISE(round(xx),round(yy),round(zz),stl_file,'xyz');  
+        for i=1:yy
+            figure
+            imagesc(squeeze(OUTPUTgrid(:,i,:)))
+            colormap(gray(2))
+            axis equal tight
+            imwrite(squeeze(OUTPUTgrid(:,i,:)),['output/array',num2str(i),'.png'])
+        end
+    case 'z'
+        %Voxelise the STL:
+        xx=xco/print_displacement;
+        yy=yco/print_displacement;
+        zz=zco/layer_displacement;
+        [OUTPUTgrid] = VOXELISE(round(xx),round(yy),round(zz),stl_file,'xyz');  
+        for i=1:zz
+            figure
+            imagesc(squeeze(OUTPUTgrid(:,:,i)))
+            colormap(gray(2))
+            axis equal tight
+            imwrite(squeeze(OUTPUTgrid(:,:,i)),['output/array',num2str(i),'.png'])
+        end
+end
